410dca2e9185f890c6ffe292a23453fe9ccb5a71
Tobi <stevehappyboy@rocketmail.com>
2020-07-08 07:05:58 +0100

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,142 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import React, {
+  createContext,
+  FunctionComponent,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
-interface Props {
-  text: string
+interface ModalProviderProps {
+  children?: any;
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+type RenderModal = (props: {
+  isOpen: boolean;
+  close: () => void;
+  children: ReactNode;
+}) => ReactNode;
+
+type ModalHandler = (
+  component: FunctionComponent,
+  props: Parameters<typeof component>[0],
+  renderModal: RenderModal,
+) => void;
+
+interface ModalHandle {
+  close: () => void;
+}
+
+let modalHandler: any = null;
+
+const components: Map<
+  string,
+  {
+    id: string;
+    Component: FunctionComponent;
+    props: any;
+    renderModal: RenderModal;
+    modalHandle?: ModalHandle;
+  }
+> = new Map();
+
+export function ModalProvider({children}: ModalProviderProps) {
+  const [currentId, setState] = React.useState<string>();
+
+  const handler: ModalHandler = (
+    Component: FunctionComponent,
+    props: Parameters<typeof Component>[0] = {},
+    renderModal: RenderModal,
+  ) => {
+    const id = new Date().toISOString() + '' + Math.random();
+    components.set(id, {id, Component, props, renderModal});
+    setState(id);
+    return {
+      close: () => {
+        const c = components.get(id);
+        if (c && c.modalHandle) {
+          c.modalHandle.close();
+        } else {
+          console.warn('modal is not ready or has already been unmounted');
+        }
+      },
+    };
+  };
+
+  modalHandler = useCallback(handler, [setState]);
+
+  return useMemo(
+    () => (
+      <>
+        {children}
+        {Array.from(components.values()).map(
+          ({id, Component, props, renderModal}) => {
+            return (
+              <Modal key={id} id={id} renderModal={renderModal}>
+                <Component {...props} />
+              </Modal>
+            );
+          },
+        )}
+      </>
+    ),
+    [currentId, components.size, children],
+  );
+}
+
+interface ModalProps {
+  id: string;
+  renderModal: RenderModal;
+}
+
+const ModalHandleContext = createContext<ModalHandle>({} as any);
+
+export const useModalHandle = (): ModalHandle => {
+  return useContext(ModalHandleContext);
+};
+
+const Modal: FunctionComponent<ModalProps> = ({id, renderModal, children}) => {
+  const [isOpen, setOpen] = useState(true);
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const modalHandle = {close: handleClose};
+
+  useEffect(() => {
+    const c = components.get(id);
+    if (c) {
+      c.modalHandle = modalHandle;
+    }
+
+    if (!isOpen) {
+      setTimeout(() => {
+        components.delete(id);
+      }, 1000);
+    }
+  }, [isOpen, modalHandle]);
+
+  return (
+    <ModalHandleContext.Provider value={modalHandle}>
+      {renderModal({isOpen, close: handleClose, children})}
+    </ModalHandleContext.Provider>
+  );
+};
+
+export function createModalHelper(renderModal: RenderModal) {
+  function modal<T extends FunctionComponent>(
+    Component: T,
+    props: Parameters<typeof Component>[0] = {},
+  ): ModalHandle {
+    if (!modalHandler) {
+      throw new Error(
+        `Ensure that you've wrapped the app with <ModalProvider/>`,
+      );
+    }
+    return modalHandler(Component, props, renderModal);
+  }
+
+  return modal;
 }
---
+++ tests/index.test.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import {createModalHelper, ModalProvider, useModalHandle} from '../src';
+import Modal from 'react-modal';
+
+const App = () => {
+  return (
+    <ModalProvider>
+      <MyComponent />
+    </ModalProvider>
+  );
+};
+
+const modal = createModalHelper(({isOpen, children}) => (
+  <Modal isOpen={isOpen} ariaHideApp={false}>
+    {children}
+  </Modal>
+));
+
+const ModalComponent = () => {
+  const {close} = useModalHandle();
+  return (
+    <div data-testid="modal-component">
+      <h1 data-testid="modal-title">Hello Modal!</h1>
+      <button data-testid="dismiss-button" onClick={close}>
+        Dismiss
+      </button>
+    </div>
+  );
+};
+
+const MyComponent = () => {
+  let modalHandle;
+  const onShowClick = () => {
+    modalHandle = modal(ModalComponent);
+  };
+
+  const onCloseClick = () => {
+    modalHandle.close();
+  };
+
+  return (
+    <>
+      <button data-testid="show-button" onClick={onShowClick}>
+        Show
+      </button>
+      <button data-testid="close-button" onClick={onCloseClick}>
+        Close
+      </button>
+    </>
+  );
+};
+
+beforeAll(() => {
+  render(<App />);
+});
+
+it('opens and closes', async () => {
+  fireEvent.click(screen.getByTestId('show-button'));
+  await waitFor(() => screen.queryByTestId('modal-component'));
+  expect(screen.getByTestId('modal-title')).toHaveTextContent('Hello Modal!');
+  await waitFor(() => screen.getByRole('dialog'));
+  fireEvent.click(screen.getByTestId('dismiss-button'));
+  expect(screen.queryByTestId('modal-component')).toBeNull();
+
+  fireEvent.click(screen.getByTestId('show-button'));
+  await waitFor(() => screen.queryByTestId('modal-component'));
+  expect(screen.getByTestId('modal-title')).toHaveTextContent('Hello Modal!');
+  await waitFor(() => screen.getByRole('dialog'));
+  fireEvent.click(screen.getByTestId('close-button'));
+  expect(screen.queryByTestId('modal-component')).toBeNull();
+});
