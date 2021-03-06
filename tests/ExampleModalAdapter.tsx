146ed0f4ac41cbddd04f454d34708c9bff229757
Tobi <stevehappyboy@rocketmail.com>
2021-03-06 20:13:36 +0000

    set options type

--- tests/ExampleModalAdapter.tsx
+++ tests/ExampleModalAdapter.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {createModalAdapter} from '../src';
 import Modal from 'react-modal';
 
-export const ExampleModalAdapter = createModalAdapter(({children, isOpen}) => {
-  return (
-    <Modal isOpen={isOpen} ariaHideApp={false}>
-      {children}
-    </Modal>
-  );
-});
+export const ExampleModalAdapter = createModalAdapter<{type: string}>(
+  ({children, isOpen, options}) => {
+    return (
+      <Modal isOpen={isOpen} ariaHideApp={false}>
+        {children}
+      </Modal>
+    );
+  },
+);
